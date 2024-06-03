16d03bce1a4ed99cdab480fe464dbb810b9323ce
Dafa ahmad sajangah <dafaahmad145@gmail.com>
2024-06-03 22:01:15 +0700

    ganti ganti nama

--- resources/views/admin/daftar_makanan/create.blade.php
+++ resources/views/admin/daftar_makanan/create.blade.php
@@ -4,36 +4,43 @@
 
 
 <div class="container mt-4">
-    <h2>Tambah Data</h2>
+    <h2>Tambah Data Warga</h2>
 
     <form action="{{ route('admin.daftarmakanan.store') }}" method="post" enctype="multipart/form-data">
         @csrf
 
         <div class="form-group my-2">
-            <label for="nama_menu">Nama Menu</label>
+            <label for="nama_menu">NIK</label>
             <input type="text" class="form-control" id="nama_menu" name="nama_menu" required>
         </div>
 
         <div class="form-group my-2">
-            <label for="harga">Harga</label>
+            <label for="nama_menu">Nama</label>
+            <input type="text" class="form-control" id="nama_menu" name="nama_menu" required>
+        </div>
+
+        <div class="form-group my-2">
+            <label for="harga">Tempat, Tanggal Lahir</label>
             <input type="text" class="form-control" id="harga" name="harga" required>
         </div>
 
         <div class="form-group my-2">
-            <label for="kategori">Kategori</label>
-            <select class="form-control" id="kategori" name="kategori" required>
-                <option value="makanan">Makanan</option>
-                <option value="minuman">Minuman</option>
-            </select>
+            <label for="kategori">Alamat</label>
+            <input type="text" class="form-control" id="" name="" required>
         </div>
 
         <div class="form-group my-2">
-            <label for="deskripsi">Deskripsi</label>
-            <textarea class="form-control" id="deskripsi" name="deskripsi" rows="3" required></textarea>
+            <label for="deskripsi">Status</label>
+            <input class="form-control" id="deskripsi" name="deskripsi" rows="3" required></input>
+        </div>
+
+        <div class="form-group my-2">
+            <label for="deskripsi">Agama</label>
+            <input class="form-control" id="deskripsi" name="deskripsi" rows="3" required></input>
         </div>
 
         <div class="form-group my-2 form-control">
-            <label for="gambar">Gambar </label>
+            <label for="gambar">Foto</label>
             <input type="file" class="form-control-file" id="gambar" name="gambar" required>
         </div>
 
--- resources/views/web/daftar_pesanan.blade.php
+++ resources/views/web/daftar_pesanan.blade.php
@@ -1,68 +1,70 @@
 @extends('layout.app')
 
 @section('content')
-   
 
-    <div class="container mt-4">
-        <h2>Daftar Pesanan</h2>
 
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>No.</th>
-                    <th>Nama Menu</th>
-                    <th>Quantity</th>
-                    <th>Harga</th>
-                    <th>Total Harga</th>
-                    <th>Aksi</th> <!-- Kolom untuk tombol tambah dan kurang -->
-                </tr>
-            </thead>
-            <tbody>
+<div class="container mt-4">
+    <h2>Daftar Pesanan</h2>
+
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>No.</th>
+                <th>NIK</th>
+                <th>Nama</th>
+                <th>Ttl</th>
+                <th>Alamat</th>
+                <th>Status</th>
+                <th>Agama</th>
+                <th>Aksi</th> <!-- Kolom untuk tombol tambah dan kurang -->
+            </tr>
+        </thead>
+        <tbody>
+            @php
+            $totalHarga = 0;
+            @endphp
+
+            {{-- Loop through daftar pesanan --}}
+            @foreach ($daftarPesanan as $index => $pesanan)
+            <tr>
+                <td>{{ $index + 1 }}</td>
+                <td>{{ $dataMenu[$index]->nama_menu }}</td>
+                <td>
+                    <div class="input-group">
+                        <span class="input-group-btn">
+                            <a href="{{ route('tambahQuantity', ['id' => $pesanan->id]) }}" class="btn btn-primary btn-sm">+</a>
+                        </span>
+                        <input type="text" class="form-control" value="{{ $pesanan->quantity }}" readonly>
+                        <span class="input-group-btn">
+                            <a href="{{ route('kurangQuantity', ['id' => $pesanan->id]) }}" class="btn btn-danger btn-sm">-</a>
+                        </span>
+                    </div>
+                </td>
+                <td>{{ $dataMenu[$index]->harga }}</td>
+                <td>{{ $pesanan->quantity * $dataMenu[$index]->harga }}</td>
                 @php
-                    $totalHarga = 0;
+                $totalHarga += $pesanan->quantity * $dataMenu[$index]->harga;
                 @endphp
+                <td>
+                    <a href="{{ route('hapusDaftarPesanan', ['index' => $index + 1]) }}" class="btn btn-danger btn-sm">Hapus</a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 
-                {{-- Loop through daftar pesanan --}}
-                @foreach ($daftarPesanan as $index => $pesanan)
-                    <tr>
-                        <td>{{ $index + 1 }}</td>
-                        <td>{{ $dataMenu[$index]->nama_menu }}</td>
-                        <td>
-                            <div class="input-group">
-                                <span class="input-group-btn">
-                                    <a href="{{ route('tambahQuantity', ['id' => $pesanan->id]) }}" class="btn btn-primary btn-sm">+</a>
-                                </span>
-                                <input type="text" class="form-control" value="{{ $pesanan->quantity }}" readonly>
-                                <span class="input-group-btn">
-                                    <a href="{{ route('kurangQuantity', ['id' => $pesanan->id]) }}" class="btn btn-danger btn-sm">-</a>
-                                </span>
-                            </div>
-                        </td>
-                        <td>{{ $dataMenu[$index]->harga }}</td>
-                        <td>{{ $pesanan->quantity * $dataMenu[$index]->harga }}</td>
-                        @php
-                            $totalHarga += $pesanan->quantity * $dataMenu[$index]->harga;
-                        @endphp
-                        <td>
-                            <a href="{{ route('hapusDaftarPesanan', ['index' => $index + 1]) }}" class="btn btn-danger btn-sm">Hapus</a>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-
-        <div class="mt-3">
-            <p>Total Harga yang Harus Dibayar di Kasir: <strong>Rp. {{ $totalHarga }}</strong></p>
-        </div>
-
-        <div class="d-flex justify-content-end">
-        <button class="btn btn-success" onclick="showMessageSuccess()">PROSES</button> 
-        </div>
+    <div class="mt-3">
+        <p>Total Harga yang Harus Dibayar di Kasir: <strong>Rp. {{ $totalHarga }}</strong></p>
     </div>
 
-    <script>
-        function     showMessageSuccess() {
-            alert('Berhasil!. Pesanan sudah diproses.')
-        }
-    </script>
+    <div class="d-flex justify-content-end">
+        <button class="btn btn-success" onclick="showMessageSuccess()">PROSES</button>
+    </div>
+</div>
+
+<script>
+    function showMessageSuccess() {
+        alert('Berhasil!. Pesanan sudah diproses.')
+    }
+</script>
 @endsection
