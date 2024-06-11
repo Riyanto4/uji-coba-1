b406409d4b3a893c6ec4d840141b71956225a060
Dafa ahmad sajangah <dafaahmad145@gmail.com>
2024-06-11 22:14:29 +0700

    update sedikit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,61 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pemesanan Makanan</title>
-    <!-- Tambahkan link Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-</head>
-<body>
-    <section class="vh-100 gradient-custom">
-        <div class="container py-5 h-100">
-          <div class="row d-flex justify-content-center align-items-center h-100">
-            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-              <div class="card bg-dark text-white" style="border-radius: 1rem;">
-                <div class="card-body p-5 text-center">
-      
-                  <div class="mb-md-5 mt-md-4 pb-5">
-      
-                    <h2 class="fw-bold mb-2 text-uppercase">Register for Data Warga</h2>
-                    <p class="text-white-50 mb-5">Please fill out the form to create an account!</p>
-      
-                    <form action="{{ route('register') }}" method="POST">
-                      @csrf
-                  
-                      <div class="form-outline form-white mb-4">
-                          <input type="text" name="username" id="typeUsername" class="form-control form-control-lg" />
-                          <label class="form-label" for="typeUsername">Username</label>
-                      </div>
-                  
-                      <div class="form-outline form-white mb-4">
-                          <input type="password" name="password" id="typePassword" class="form-control form-control-lg" />
-                          <label class="form-label" for="typePassword">Password</label>
-                      </div>
-                  
-                      <div class="form-outline form-white mb-4">
-                          <input type="password" name="password_confirmation" id="typeConfirmPassword" class="form-control form-control-lg" />
-                          <label class="form-label" for="typeConfirmPassword">Konfirmasi Password</label>
-                      </div>
-                      @error('password')
-                      <div class="alert alert-danger">{{ $message }}</div>
-                      @enderror
 
-                  
-                      <button class="btn btn-outline-light btn-lg px-5" type="submit">Daftar</button>
-                  </form>
-                  
-      
-                    <!-- <a href="{{ route('login') }}" class="text-white-50 fw-bold">Login</a> -->
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pemesanan Makanan</title>
+  <!-- Tambahkan link Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+  <section class="vh-100 gradient-custom">
+    <div class="container py-5 h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+          <div class="card bg-dark text-white" style="border-radius: 1rem;">
+            <div class="card-body p-5 text-center">
+
+              <div class="mb-md-5 mt-md-4 pb-5">
+
+                <h2 class="fw-bold mb-2 text-uppercase">Buat Akun Data Warga</h2>
+                <p class="text-white-50 mb-5">Tolong isi kolom untuk membuat akun!</p>
+
+                <form action="{{ route('register') }}" method="POST">
+                  @csrf
+
+                  <div class="form-outline form-white mb-4">
+                    <input type="text" name="username" id="typeUsername" class="form-control form-control-lg" />
+                    <label class="form-label" for="typeUsername">Username</label>
                   </div>
-                </div>
+
+                  <div class="form-outline form-white mb-4">
+                    <input type="password" name="password" id="typePassword" class="form-control form-control-lg" />
+                    <label class="form-label" for="typePassword">Password</label>
+                  </div>
+
+                  <div class="form-outline form-white mb-4">
+                    <input type="password" name="password_confirmation" id="typeConfirmPassword" class="form-control form-control-lg" />
+                    <label class="form-label" for="typeConfirmPassword">Konfirmasi Password</label>
+                  </div>
+                  @error('password')
+                  <div class="alert alert-danger">{{ $message }}</div>
+                  @enderror
+
+
+                  <button class="btn btn-outline-light btn-lg px-5" type="submit">Daftar</button>
+                </form>
+                <br>
+
+                <p>Sudah Ada Akun? <a href="{{ route('login') }}" class="text-white-50 fw-bold">Login</a></p>
+
+                <!-- <a href="{{ route('login') }}" class="text-white-50 fw-bold">Login</a> -->
               </div>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+    </div>
+  </section>
 
-        <!-- Tambahkan script Bootstrap JavaScript (Popper.js dan Bootstrap JS) -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <!-- Tambahkan script Bootstrap JavaScript (Popper.js dan Bootstrap JS) -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
+
 </html>
--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -3,22 +3,55 @@
 @section('content')
 
 <div class="container mt-5">
-    <h2 class="mb-4">Menu Makanan</h2>
+    <h2 class="mb-4">Data Sensus Warga</h2>
     <div class="row">
-        @foreach($menus as $menu)
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <!-- Ganti URL gambar dengan path sesuai dengan struktur penyimpanan gambar Anda -->
-                    <img src="{{ asset('foto/' . $menu->image) }}" class="card-img-top" alt="{{ $menu->nama_menu }}">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $menu->nama_menu }}</h5>
-                        <p class="card-text">{{ $menu->deskripsi }}</p>
-                        <p class="card-text">Rp. {{ $menu->harga }}</p>
-                        <a href="{{ route('menu.addToCart', ['id' => $menu->id]) }}" class="btn btn-primary">Pesan Sekarang</a>
-                    </div>
+        <table class="table table-bordered mt-3">
+            <thead class="thead-dark">
+                <tr>
+                    <th>NIK</th>
+                    <th>Nama</th>
+                    <th>Tempat Lahir</th>
+                    <th>Tanggal Lahir</th>
+                    <th>Alamat</th>
+                    <th>Status</th>
+                    <th>Agama</th>
+                    <th>Foto</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($menus as $menu)
+                <tr>
+                    <td>{{ $menu->NIK }}</td>
+                    <td>
+                        <!-- Menampilkan gambar menggunakan tag img -->
+                        <img src="{{ asset('foto/' . $menu->image) }}" alt="{{ $menu->nama_menu }}" style="max-width: 50px;">
+                    </td>
+                    <td>{{ $menu->Nama }}</td>
+                    <td>{{ $menu->Tempat_Lahir }}</td>
+                    <td>{{ $menu->Tanggal_Lahir }}</td>
+                    <td>{{ $menu->Alamat }}</td>
+                    <td>{{ $menu->Status }}</td>
+                    <td>{{ $menu->Agama }}</td>
+
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        <!-- @foreach($menus as $menu)
+        <div class="col-md-4 mb-4">
+            <div class="card"> -->
+        <!-- Ganti URL gambar dengan path sesuai dengan struktur penyimpanan gambar Anda -->
+        <!-- <img src="{{ asset('foto/' . $menu->image) }}" class="card-img-top" alt="{{ $menu->nama_menu }}">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $menu->nama_menu }}</h5>
+                    <p class="card-text">{{ $menu->deskripsi }}</p>
+                    <p class="card-text">Rp. {{ $menu->harga }}</p>
+                    <a href="{{ route('menu.addToCart', ['id' => $menu->id]) }}" class="btn btn-primary">Pesan Sekarang</a>
                 </div>
             </div>
-        @endforeach
+        </div>
+        @endforeach -->
     </div>
 </div>
 
