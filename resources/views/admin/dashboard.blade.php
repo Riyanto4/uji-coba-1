e1acb03030c8ecc7d37e8e2b7582e01a86873ae2
Dafa ahmad sajangah <dafaahmad145@gmail.com>
2024-06-13 20:41:47 +0700

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -14,7 +14,7 @@
                 <img src="{{ asset('foto/231208110259.jpg')}}" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="{{ asset('foto/231209100703.jpg')}}" class="d-block w-100" alt="...">
+                <img src="{{ asset('foto/231210030450.jpg')}}" class="d-block w-100" alt="...">
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
