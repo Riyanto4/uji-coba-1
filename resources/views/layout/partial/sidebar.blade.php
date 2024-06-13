67dde986b8c728d4ef4542ea877fb4c41d3bd1dc
Dafa ahmad sajangah <dafaahmad145@gmail.com>
2024-06-13 19:39:52 +0700

    update sidebar

--- resources/views/layout/partial/sidebar.blade.php
+++ resources/views/layout/partial/sidebar.blade.php
@@ -1,31 +1,15 @@
 @if(Auth::check() && Auth::user()->username == "Admin")
 <div class="d-flex" id="wrapper">
-            <!-- Sidebar-->
-            <div class="border-end bg-white" id="sidebar-wrapper">
-               
-                <div class="list-group list-group-flush">
-                    <a
-                        class="list-group-item list-group-item-action list-group-item-light p-3"
-                        href="{{ route('admin.dashboard') }}"
-                        >Dashboard</a
-                    >
-                    <a
-                        class="list-group-item list-group-item-action list-group-item-light p-3"
-                        href="{{ route('admin.daftarmakanan') }}"
-                        >Daftar Warga</a
-                    >
-                   <a
-                        class="list-group-item list-group-item-action list-group-item-light p-3"
-                        href="{{ route('admin.pembayaran') }}"
-                        >Pembayaran</a
-                    >
-                    <a
-                        class="list-group-item list-group-item-action list-group-item-light p-3"
-                        href="{{ route('admin.laporan') }}"
-                        >Laporan Penjualan</a
-                    >
-                </div>
-            </div>
-            <!-- Page content wrapper-->
-            @endif
+    <!-- Sidebar-->
+    <div class="border-end bg-white" id="sidebar-wrapper">
 
+        <div class="list-group list-group-flush">
+            <a class="list-group-item list-group-item-action list-group-item-light p-3" href="{{ route('admin.dashboard') }}">Dashboard</a>
+            <a class="list-group-item list-group-item-action list-group-item-light p-3" href="{{ route('admin.daftarmakanan') }}">Daftar Warga</a>
+            <a class="list-group-item list-group-item-action list-group-item-light p-3" href="{{ route('admin.pembayaran') }}">Pembayaran</a>
+            <a class="list-group-item list-group-item-action list-group-item-light p-3" href="{{ route('admin.laporan') }}">About Team</a>
+
+        </div>
+    </div>
+    <!-- Page content wrapper-->
+    @endif
