680fd147a1389475468f631bf481e8c1b69cc84d
Dafa ahmad sajangah <dafaahmad145@gmail.com>
2024-06-13 20:25:49 +0700

    tambah about

--- resources/views/admin/laporan/index.blade.php
+++ resources/views/admin/laporan/index.blade.php
@@ -6,7 +6,7 @@
     <div class="row">
         <div class="col-md-4 mb-4">
             <div class="card">
-                <img src="{{ asset('foto/FotoDafa.jpg')}}" class="card-img-top" alt="">
+                <img src="{{ asset('foto/FotoDafa.jpg')}}" class="card-img-top" alt="" width="100%" height="550px">
                 <div class="card-body">
                     <h5 class="card-title">Nama : Dafa Ahmad Sajangah</h5>
                     <p class="card-text">NIM : STI202202731</p>
@@ -16,7 +16,7 @@
         </div>
         <div class="col-md-4 mb-4">
             <div class="card">
-                <img src="{{ asset('foto/FotoDafa.jpg')}}" class="card-img-top" alt="">
+                <img src="{{ asset('foto/FotoDani.png')}}" class="card-img-top" alt="" width="100%" height="550px">
                 <div class="card-body">
                     <h5 class="card-title">Nama : Nurdani Dwi Romadon</h5>
                     <p class="card-text">NIM : STI202203300</p>
@@ -26,7 +26,7 @@
         </div>
         <div class="col-md-4 mb-4">
             <div class="card">
-                <img src="{{ asset('foto/FotoDafa.jpg')}}" class="card-img-top" alt="">
+                <img src="{{ asset('foto/FotoIkhwan.jpg')}}" class="card-img-top" alt="" width="100%" height="550px">
                 <div class="card-body">
                     <h5 class="card-title">Nama : Ikhwan Aji Ginanjar</h5>
                     <p class="card-text">NIM : STI202203295</p>
@@ -36,7 +36,7 @@
         </div>
         <div class="col-md-4 mb-4">
             <div class="card">
-                <img src="{{ asset('foto/FotoDafa.jpg')}}" class="card-img-top" alt="">
+                <img src="{{ asset('foto/FotoRiyanto.jpg')}}" class="card-img-top" alt="" width="100%" height="550px">
                 <div class="card-body">
                     <h5 class="card-title">Nama : Riyanto</h5>
                     <p class="card-text">NIM : STI202202753</p>
@@ -46,7 +46,7 @@
         </div>
         <div class="col-md-4 mb-4">
             <div class="card">
-                <img src="{{ asset('foto/FotoDafa.jpg')}}" class="card-img-top" alt="">
+                <img src="{{ asset('foto/FotoMega.jpg')}}" class="card-img-top" alt="" width="100%" height="550px">
                 <div class="card-body">
                     <h5 class="card-title">Nama : Mega Silvia</h5>
                     <p class="card-text">NIM : STI202202731</p>
